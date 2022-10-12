34bebb7aa04d60f8423d98a046ba887c1770cf5b
Brennan Cox <bhcox1@radford.edu>
2022-10-11 20:08:26 -0400

    Added tailwind styling

--- strongly-typed/pages/index.tsx
+++ strongly-typed/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="bg-blue-400">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
