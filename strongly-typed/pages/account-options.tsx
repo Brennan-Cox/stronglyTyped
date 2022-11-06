f601d9ede84cde4fcf013caa513bcd28ee9b91c2
KevinKirkham <kkirkham@radford.edu>
2022-11-06 14:05:56 -0500

    Added confirm password popup to account options page

--- strongly-typed/pages/account-options.tsx
+++ strongly-typed/pages/account-options.tsx
@@ -5,6 +5,7 @@ import { useState, FormEvent } from 'react'
 import { signIn, getSession, signOut, getCsrfToken } from 'next-auth/react'
 import { Session } from 'next-auth';
 
+
 /**
  * Account Options is currently a page deticated to allow for password and username changing
  * In order for a user to update either their password or Username they should enter their password
@@ -34,9 +35,9 @@ const AccountOptions: NextPage = () => {
      * 
      * @param event: FormEvent<HTMLFormElement>
      */
-    async function handleUpdatePass(event: FormEvent<HTMLFormElement>) {
+    async function handleUpdatePass() {
 
-        event.preventDefault()
+        // event.preventDefault()
         setError('')
 
         // change the user password of a user in the database
@@ -63,6 +64,7 @@ const AccountOptions: NextPage = () => {
             var errorResponse = await results.json()
             setError(errorResponse.error)
         }
+        togglePopup("")
     }
 
     /**
@@ -71,9 +73,9 @@ const AccountOptions: NextPage = () => {
      * On success, a success message will be displayed
      * @param event 
      */
-    async function handleUpdateUser(event: FormEvent<HTMLFormElement>) {
+    async function handleUpdateUser() {
 
-        event.preventDefault()
+        // event.preventDefault()
         setError('')
 
         // change the username where a valid password is given
@@ -98,6 +100,7 @@ const AccountOptions: NextPage = () => {
             var errorResponse = await results.json()
             setError(errorResponse.error)
         }
+        togglePopup("")
     }
 
     /**
@@ -139,9 +142,47 @@ const AccountOptions: NextPage = () => {
         }
     }
 
+    // This is for the popup
+    const [popup, setPopup] = useState(false);
+    const [calledFrom, setCalledFrom] = useState('');
+    function togglePopup(calledFrom : string) {
+        setCalledFrom(calledFrom);
+        if (calledFrom == "password") {
+            if (newPassword != confirmNewPassword) {
+                setError("Password and confirm password doesn't match")
+                return
+            }
+        }
+        setPopup(!popup);
+    }
+
+    function updatePushed() {
+        if (calledFrom == "username") {
+            handleUpdateUser()
+        }
+        else if (calledFrom == "password") {
+            handleUpdatePass()
+        }
+    }
+
     return (
         <main className="bg-stgray-100 h-screen flex justify-center items-center">
-            <div className="w-128">
+
+            {/* This is the popup div, displays if popup is true */}
+            <div id="popup" className={popup ? "py-7 items-center w-100 bg-stgray-200 static" : "hidden"}>
+                <div className="grid grid-cols-3">
+                    <button onClick={() => togglePopup("")}><svg className="ml-10 w-10 h-10 text-mint" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg></button>
+                    <h4 className="text-white text-2xl">Input password</h4>
+                </div>
+                <div className="flex ml-4 justify-center mt-3">
+                    <input type="password" onChange={(e) => setCurrentPassword(e.target.value)} className="text-center h-8 w-64" />
+                    <button type="submit" onClick={() => updatePushed()} className="text-mint text-center py-1 font-semibold border-2 border-mint hover:bg-stgray-100 w-40 mr-10 ml-auto">Update</button>
+                </div>
+            </div>
+
+            {/* This is the main page, displays if popup is false  */}
+            <div className={popup ? "hidden" : "w-128"}>
+                {/* <button className="border-4 text-white" onClick={() => togglePopup()}>Here is a button</button> */}
                 <div className="bg-stgray-200 rounded-sm">
                     <div className="grid grid-cols-3">
                         <div className="pl-10 pt-10">
@@ -157,32 +198,27 @@ const AccountOptions: NextPage = () => {
                     <div className="flex justify-center">
                         <h4 className="login-text text-white">Edit Account</h4>
                     </div>
+
                     {/*Area to print success or error message */}
                     <ErrorMessage />
                     <SuccessMessage />
-                    {/*Area that grabs current password and 'is required' */}
-                    <div className="flex justify-evenly">
-                        <div>
-                            <p className="mt-3 mb-2 text-mint text-xl text-center">Current Password Required for Edits</p>
-                            <input onChange={(e) => setCurrentPassword(e.target.value)} type="password" id="password" name="password" className="w-96 rounded-sm mb-3" required />
-                        </div>
-                    </div>
+                    
                     {/*Form intended to update the username */}
-                    <form id="updateUser" onSubmit={(e) => { handleUpdateUser(e) }}>
+                    <form id="updateUser" onSubmit={(e) => { e.preventDefault() }}>
                         <h1 className="ml-6 mt-3 mb-2 text-white">New Username</h1>
                         <input onChange={(e) => setNewUsername(e.target.value)} type="text" id="username" name="username" className="w-96 ml-6 rounded-sm mb-6" />
                         <div className="ml-6">
-                            <button type="submit" className="text-mint text-center py-1 font-semibold border-2 border-mint hover:bg-stgray-100 w-40 mr-10 ml-auto">Update Username</button>
+                            <button type="submit" onClick={() => togglePopup("username")} className="text-mint text-center py-1 font-semibold border-2 border-mint hover:bg-stgray-100 w-40 mr-10 ml-auto">Update Username</button>
                         </div>
                     </form>
                     {/*Form intended to update the password */}
-                    <form id="updatePassword" onSubmit={(e) => { handleUpdatePass(e) }}>
+                    <form id="updatePassword" onSubmit={(e) => { e.preventDefault() }}>
                         <h1 className="ml-6 mt-3 pb-2 text-white">New Password</h1>
                         <input onChange={(e) => setNewPassword(e.target.value)} type="password" id="confirm_password" name="confirm_password" className="w-96 ml-6 rounded-sm mb-6" required />
                         <h1 className="ml-6 mt-3 pb-2 text-white">Confirm New Password</h1>
                         <input onChange={(e) => confirmPassword(e.target.value)} type="password" id="confirm_password" name="confirm_password" className="w-96 ml-6 rounded-sm mb-6" required />
                         <div className="pb-4 ml-6">
-                            <button type="submit" className="text-mint text-center py-1 font-semibold border-2 border-mint hover:bg-stgray-100 w-40 mr-10">Update Password</button>
+                            <button type="submit" onClick={() => togglePopup("password")} className="text-mint text-center py-1 font-semibold border-2 border-mint hover:bg-stgray-100 w-40 mr-10">Update Password</button>
                         </div>
                     </form>
                 </div>
